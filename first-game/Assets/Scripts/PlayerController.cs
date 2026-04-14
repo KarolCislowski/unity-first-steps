5a036cfbe426923bfa8ac5a2ce7b631f3c49e944
Karol Cislowki <cislowski.karol@hotmail.com>
2026-04-14 13:14:59 +0200

    Refactor PlayerController to improve movement handling and structure

--- first-game/Assets/Scripts/PlayerController.cs
+++ first-game/Assets/Scripts/PlayerController.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
 
-        transform.Translate(new Vector3(moveX, moveY, 0) * speed * Time.deltaTime);
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
